359ca1a8ed2f2c0254ef89a77b451294d8afca04
yang07 <rhrlajrwk56@gmail.com>
2026-08-30 17:25:59 +0900

    Widen physical torsion support

--- gpuopt/candidates/torsion_assimilator.py
+++ gpuopt/candidates/torsion_assimilator.py
@@ -49,12 +49,12 @@ TORSION_COLUMNS = tuple(
 ESM_MODEL = "esm2_t30_150M_UR50D"
 ESM_DIM = 640
 ACTUATOR_SPECS = (
-    ("phi", 0.60),
-    ("psi", 0.60),
-    ("chi1", 1.50),
-    ("chi2", 1.50),
-    ("chi3", 1.50),
-    ("chi4", 1.50),
+    ("phi", 0.80),
+    ("psi", 0.80),
+    ("chi1", 2.00),
+    ("chi2", 2.00),
+    ("chi3", 2.00),
+    ("chi4", 2.00),
 )
 SIDECHAIN_CHI_BONDS = {
     "ARG": (("CA", "CB"), ("CB", "CG"), ("CG", "CD"), ("CD", "NE")),
